27aa7306b40eb5f717aee3b8c58123ac8f9c4b79
PudFish <chook.myers@gmail.com>
2024-10-01 14:10:13 +1000

    add tag type

--- nbt.go
+++ nbt.go
@@ -1,6 +1,8 @@
 // Package nbt enables robust reading and writing of Minecraft named binary tags (NBT) files.
 package nbt
 
+import "fmt"
+
 // tag types and IDs: source https://minecraft.fandom.com/wiki/NBT_format.
 const (
 	tagEnd       uint8 = 0
@@ -22,3 +24,59 @@ const (
 // edition, all numbers are encoded in big-endian. In Minecraft Bedrock edition, all numbers are encoded in
 // little-endian.
 // var globalByteOrder binary.ByteOrder = binary.LittleEndian
+
+// tag is the custom type to hold common information of each tag type, with a generic payload capacity. Most tag
+// payloads are the expected type.
+// tagEnd: N/A, no payload
+// tagByte: byte
+// tagShort: int16
+// tagInt: int32
+// tagLong: int64
+// tagFloat: float32
+// tagDouble: float64
+// tagByteArray: []byte
+// tagString: string
+// tagList: []any, assumes the type of listed tags
+// tagCompound: []*tag, representing child tags an omitting the tagEnd
+// tagIntArray: []int32
+// tagLongArray: []int64
+type tag struct {
+	id      uint8
+	name    string
+	payload any
+}
+
+// tagType returns the name associated with the tag ID
+func (t *tag) tagType() (tagType string, err error) {
+	switch t.id {
+	case tagEnd:
+		tagType = "tagEnd"
+	case tagByte:
+		tagType = "tagByte"
+	case tagShort:
+		tagType = "tagShort"
+	case tagInt:
+		tagType = "tagInt"
+	case tagLong:
+		tagType = "tagLong"
+	case tagFloat:
+		tagType = "tagFloat"
+	case tagDouble:
+		tagType = "tagDouble"
+	case tagByteArray:
+		tagType = "tagByteArray"
+	case tagString:
+		tagType = "tagString"
+	case tagList:
+		tagType = "tagList"
+	case tagCompound:
+		tagType = "tagCompound"
+	case tagIntArray:
+		tagType = "tagIntArray"
+	case tagLongArray:
+		tagType = "tagLongArray"
+	default:
+		err = fmt.Errorf("tag ID %v not between 0 (tagEnd) and 12 (tagLongArray)", t.id)
+	}
+	return tagType, err
+}
--- nbt_test.go
+++ nbt_test.go
@@ -1,2 +1,45 @@
 // Package nbt enables robust reading and writing of Minecraft named binary tags (NBT) files.
 package nbt
+
+import "testing"
+
+func TestTagType(t *testing.T) {
+	successCases := []struct {
+		name string
+		wantTagType string
+		t tag
+	}{
+		{"tagEnd (0)", "tagEnd", tag{0, "", nil}},
+		{"tagByte (1)", "tagByte", tag{1, "", nil}},
+		{"tagShort (2)", "tagShort", tag{2, "", nil}},
+		{"tagInt (3)", "tagInt", tag{3, "", nil}},
+		{"tagLong (4)", "tagLong", tag{4, "", nil}},
+		{"tagFloat (5)", "tagFloat", tag{5, "", nil}},
+		{"tagDouble (6)", "tagDouble", tag{6, "", nil}},
+		{"tagByteArray (7)", "tagByteArray", tag{7, "", nil}},
+		{"tagString (8)", "tagString", tag{8, "", nil}},
+		{"tagList (9)", "tagList", tag{9, "", nil}},
+		{"tagCompound (10)", "tagCompound", tag{10, "", nil}},
+		{"tagIntArray (11)", "tagIntArray", tag{11, "", nil}},
+		{"tagLongArray (12)", "tagLongArray", tag{12, "", nil}},
+	}
+	for _, successCase := range successCases {
+		t.Run("Test success case: "+successCase.name, func(t *testing.T) {
+			gotTagType, gotErr := successCase.t.tagType()
+			if gotTagType != successCase.wantTagType {
+				t.Errorf("got %v, want %v", gotTagType, successCase.wantTagType)
+			}
+			if gotErr != nil {
+				t.Errorf("got %v, want nil", gotErr)
+			}
+		})
+	}
+
+	t.Run("Test failure case: tag id out of range", func(t *testing.T) {
+		failTag := tag{13, "", nil}
+		_, gotErr := failTag.tagType()
+		if gotErr == nil {
+			t.Errorf("got nil, want non-nil")
+		}
+	})
+}
